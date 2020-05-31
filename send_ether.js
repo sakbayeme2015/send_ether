e99a124bdbc1234c4b2320cde9ce06e92fbe3fff
sakbayeme2015 <sakbayeme2015@gmail.com>
2020-05-31 16:34:57 -0400

    Create send_ether.js

---
+++ send_ether.js
@@ -0,0 +1,32 @@
+var Tx     = require('ethereumjs-tx')
+const Web3 = require('web3')
+const web3 = new Web3('http://localhost:8545')
+
+const account1 = '' 
+const account2 = '' 
+
+const privateKey1 = Buffer.from('', 'hex')
+
+web3.eth.getTransactionCount(account1, (err, txCount) => {
+  // Build the transaction
+  const txObject = {
+    nonce:    web3.utils.toHex(txCount),
+    to:       account2,
+    value:    web3.utils.toHex(web3.utils.toWei('5', 'ether')),
+    gasLimit: web3.utils.toHex(21000),
+    gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
+  }
+
+  // Sign the transaction
+  const tx = new Tx(txObject)
+  tx.sign(privateKey1)
+
+  const serializedTx = tx.serialize()
+  const raw = '0x' + serializedTx.toString('hex')
+
+  // Broadcast the transaction
+  web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+    console.log('txHash:', txHash)
+    // Now go check etherscan to see the transaction!
+  })
+}) 
